2a077a7b4ff1654c28cf7ec1b2c8e51c164a4f94
ahnet-69 <syedmusicrealise@gmail.com>
2024-04-25 18:02:35 +0000

    pdx206: Nuke unneeded patches
    Its finally there lol

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Resetting repos to head state"
-cd hardware/google/pixel
-git reset --hard FETCH_HEAD
-echo "Applying patches"
-git am ../../../device/sony/pdx206/patches/platform_hardware_google_pixel/0001-kernel_headers-Guard-with-soong-namespace.patch
-cd ../../..
-echo "done"
